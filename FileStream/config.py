39d0026557026dd1d67a82410286bac3897ea080
humtpaglu <jesuschristusa456@gmail.com>
2025-05-18 22:39:56 +0530

    Update config.py

--- FileStream/config.py
+++ FileStream/config.py
@@ -6,10 +6,10 @@ load_dotenv()
 class Telegram:
     API_ID = int(env.get("API_ID", '23897874'))
     API_HASH = str(env.get("API_HASH", 'ec91dd01da9693911a6ee4af5d0bef2c'))
-    BOT_TOKEN = str(env.get("BOT_TOKEN", '6163661049:AAGxi6BF4dCkH6beGD0Jxwrpj9SeolCCRfs'))
+    BOT_TOKEN = str(env.get("BOT_TOKEN", '7781615462:AAHT9qKJyhFTU2GqqbmUjF8l8etaJrjjEww'))
     OWNER_ID = int(env.get('OWNER_ID', '1572626591'))
     WORKERS = int(env.get("WORKERS", "6"))  # 6 workers = 6 commands at once
-    DATABASE_URL = str(env.get('DATABASE_URL', 'mongodb+srv://hackergaurav02:harsh0711@cluster0.xlfo88p.mongodb.net/?retryWrites=true&w=majority'))
+    DATABASE_URL = str(env.get('DATABASE_URL', 'mongodb+srv://thehopea14:Lastfix@cluster0.4tia8bq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'))
     UPDATES_CHANNEL = str(env.get('UPDATES_CHANNEL', "Max_Leech_Zone_Update"))
     SESSION_NAME = str(env.get('SESSION_NAME', 'FileStream'))
     FORCE_SUB_ID = env.get('FORCE_SUB_ID', '-1001838333552')
@@ -20,8 +20,8 @@ class Telegram:
     START_PIC = env.get('START_PIC', "https://envs.sh/nQw.jpg")
     VERIFY_PIC = env.get('VERIFY_PIC', "https://graph.org/file/736e21cc0efa4d8c2a0e4.jpg")
     MULTI_CLIENT = False
-    FLOG_CHANNEL = int(env.get("FLOG_CHANNEL", '-1002073550724'))   # Logs channel for file logs
-    ULOG_CHANNEL = int(env.get("ULOG_CHANNEL", '-1002073550724'))   # Logs channel for user logs
+    FLOG_CHANNEL = int(env.get("FLOG_CHANNEL", '-1002637340403'))   # Logs channel for file logs
+    ULOG_CHANNEL = int(env.get("ULOG_CHANNEL", '-1002637340403'))   # Logs channel for user logs
     MODE = env.get("MODE", "primary")
     SECONDARY = True if MODE.lower() == "secondary" else False
     AUTH_USERS = list(set(int(x) for x in str(env.get("AUTH_USERS", "")).split()))
